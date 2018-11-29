9a961e05059932d63b4391b781dad936c7cd39f8
pakichu <smolguppy@gmail.com>
2018-11-29 07:41:35 -0600

    ip & hostname command added

--- elm.py
+++ elm.py
@@ -9,16 +9,32 @@ print('Type "help" (without quatation) for a list of commands')
 #help func
 def elmHelp():
     print('exit- exits elm cli')
+    print('cls- clears the screen')
+    print('ip- gets local ip')
+    print('hostname- get local hostname')
 #clear output func
 def cls():
-    print('\n' * 50)
+    print('\n' * 10)
+#getip func
+def getIp():
+    print(hostIP)
+#gethostname func
+def getHostname():
+    print(host)
 while True:
     command = input()
     #check if the command is help
     if command == 'help':
         cls()
         elmHelp()
-    #check if the command is exit and if it is exit the CLI
+    #check if the command is exit and if it is exit elm
     if command == 'exit':
         exit()
+    #check if command is cls
+    if command == 'cls':
+        cls()
+    if command == 'ip':
+        getIp()
+    if command == 'hostname':
+        getHostname()
 
