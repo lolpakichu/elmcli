1eb2bd1e36c396a8725993158390b3e8f1db1d2a
pakichu <smolguppy@gmail.com>
2018-11-29 12:03:13 -0600

    rand command added

--- elm.py
+++ elm.py
@@ -1,8 +1,10 @@
 import socket
+import random
 
 host = socket.gethostname()
 hostIP = socket.gethostbyname(host)
-
+arg1 = '1'
+arg2 = '1'
 print(host,'@',hostIP, '\n','WELCOME TO ELM A LIGHTWEIGHT CONSOLE')
 
 print('Type "help" (without quatation) for a list of commands')
@@ -12,6 +14,7 @@ def elmHelp():
     print('cls- clears the screen')
     print('ip- gets local ip')
     print('hostname- get local hostname')
+    print('rand- generates a random number')
 #clear output func
 def cls():
     print('\n' * 8)
@@ -21,6 +24,13 @@ def getIp():
 #gethostname func
 def getHostname():
     print(host)
+#rand func
+def Rand(minimum, maximum):
+    print('Input your minimum number')
+    minimum = input()
+    print('Input your maximum number')
+    maximum = input()
+    print(random.randint(int(minimum), int(maximum)))
 while True:
     command = input()
     #check if the command is help
@@ -37,4 +47,5 @@ while True:
         getIp()
     if command == 'hostname':
         getHostname()
-
+    if command == 'rand':
+        Rand(arg1, arg2)
