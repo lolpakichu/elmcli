e2edec9f6b05471bb1075ec8e7f591d866999dfb
pakichu <smolguppy@gmail.com>
2018-12-31 20:17:14 -0700

    remove lastcommand

--- elm.py
+++ elm.py
@@ -8,8 +8,6 @@ hostIP = socket.gethostbyname(host)
 # args for commands
 arg1 = '1'
 arg2 = '1'
-# last command boolean
-lastCommand = 'none'
 # startup text
 print(host,'@',hostIP, '\n','WELCOME TO ELM A LIGHTWEIGHT CONSOLE')
 print('Type "help" (without quatation) for a list of commands')
@@ -41,7 +39,6 @@ def helpCommand():
     if command == 'help':
         cls()
         elmHelp()
-        lastCommand = 'help'
 def exitCommand():
     # check if the command is exit and if it is exit elm
     if command == 'exit':
@@ -50,22 +47,18 @@ def clsCommand():
     # check if command is cls
     if command == 'cls':
         cls()
-        lastCommand = 'cls'
 def getIpCommand():
     # check if command is ip
     if command == 'ip':
         getIp()
-        lastCommand = 'ip'
 def getHostnameCommand():
     # check if command is hostname
     if command == 'hostname':
         getHostname()
-        lastCommand = 'hostname'
 def randCommand():
     # check if command is rand
     if command == 'rand':
         Rand(arg1, arg2)
-        lastCommand = 'rand'
 while True:
     command = input()
     helpCommand()
@@ -74,5 +67,3 @@ while True:
     getIpCommand()
     getHostnameCommand()
     randCommand()
-    if command == 'lastCommand':
-        command = lastCommand
